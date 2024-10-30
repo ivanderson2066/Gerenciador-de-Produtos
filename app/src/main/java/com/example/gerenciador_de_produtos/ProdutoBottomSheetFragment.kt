0234a569abb1fbe855a7f9f6e66d10325a7c9af2
luffy <elyguterres@hotmsil.com>
2024-10-30 14:02:55 -0300

    ajustes no layout, ajuste necessario nos avisos

--- app/src/main/java/com/example/gerenciador_de_produtos/ProdutoBottomSheetFragment.kt
+++ app/src/main/java/com/example/gerenciador_de_produtos/ProdutoBottomSheetFragment.kt
@@ -16,33 +16,32 @@ class ProdutoBottomSheetFragment(private val produto: Produto) : BottomSheetDial
         savedInstanceState: Bundle?
     ): View? {
         val bottomSheetView = inflater.inflate(R.layout.bottom_sheet_layout, container, false)
-
+        val descricaoProduto: TextView = bottomSheetView.findViewById(R.id.et_descricao_produto)
         val nomeProdutoPopup: TextView = bottomSheetView.findViewById(R.id.nome_produto_popup)
         val categoriaProdutoPopup: TextView = bottomSheetView.findViewById(R.id.categoria_produto_popup)
         val precoProdutoPopup: TextView = bottomSheetView.findViewById(R.id.preco_produto_popup)
         val quantidadeProdutoPopup: TextView = bottomSheetView.findViewById(R.id.quantidade_produto_popup)
-        val descricaoProdutoPopup: TextView = bottomSheetView.findViewById(R.id.et_descricao_produto)
         val validadeProdutoPopup: TextView = bottomSheetView.findViewById(R.id.validade_produto_popup)
         val entradaButtonPopup: Button = bottomSheetView.findViewById(R.id.entrada_button_popup)
         val saidaButtonPopup: Button = bottomSheetView.findViewById(R.id.saida_button_popup)
 
         // Inicializa o conteúdo com os dados atuais do produto
-        atualizarConteudoProduto(nomeProdutoPopup, categoriaProdutoPopup, precoProdutoPopup, quantidadeProdutoPopup, validadeProdutoPopup)
+        atualizarConteudoProduto(nomeProdutoPopup, categoriaProdutoPopup, precoProdutoPopup, quantidadeProdutoPopup,validadeProdutoPopup,descricaoProduto)
 
         // Configura o clique no botão de Entrada
         entradaButtonPopup.setOnClickListener {
             when (requireActivity()) {
                 is MainActivity -> (requireActivity() as MainActivity).showEntradaDialog(produto) { novaQuantidade ->
                     produto.quantidade = novaQuantidade
-                    atualizarConteudoProduto(nomeProdutoPopup, categoriaProdutoPopup, precoProdutoPopup, quantidadeProdutoPopup, validadeProdutoPopup)
+                    atualizarConteudoProduto(nomeProdutoPopup, categoriaProdutoPopup, precoProdutoPopup, quantidadeProdutoPopup, descricaoProduto,validadeProdutoPopup)
                 }
                 is BuscaActivity -> (requireActivity() as BuscaActivity).showEntradaDialog(produto) { novaQuantidade ->
                     produto.quantidade = novaQuantidade
-                    atualizarConteudoProduto(nomeProdutoPopup, categoriaProdutoPopup, precoProdutoPopup, quantidadeProdutoPopup, validadeProdutoPopup)
+                    atualizarConteudoProduto(nomeProdutoPopup, categoriaProdutoPopup, precoProdutoPopup, descricaoProduto, quantidadeProdutoPopup, validadeProdutoPopup)
                 }
                 is ProdutosActivity -> (requireActivity() as ProdutosActivity).showEntradaDialog(produto) { novaQuantidade ->
                     produto.quantidade = novaQuantidade
-                    atualizarConteudoProduto(nomeProdutoPopup, categoriaProdutoPopup, precoProdutoPopup, quantidadeProdutoPopup, validadeProdutoPopup)
+                    atualizarConteudoProduto(nomeProdutoPopup, categoriaProdutoPopup, precoProdutoPopup, quantidadeProdutoPopup, descricaoProduto ,validadeProdutoPopup)
                 }
             }
         }
@@ -52,15 +51,15 @@ class ProdutoBottomSheetFragment(private val produto: Produto) : BottomSheetDial
             when (requireActivity()) {
                 is MainActivity -> (requireActivity() as MainActivity).showSaidaDialog(produto) { novaQuantidade ->
                     produto.quantidade = novaQuantidade
-                    atualizarConteudoProduto(nomeProdutoPopup, categoriaProdutoPopup, precoProdutoPopup, quantidadeProdutoPopup, validadeProdutoPopup)
+                    atualizarConteudoProduto(nomeProdutoPopup, categoriaProdutoPopup, precoProdutoPopup, quantidadeProdutoPopup,descricaoProduto, validadeProdutoPopup)
                 }
                 is BuscaActivity -> (requireActivity() as BuscaActivity).showSaidaDialog(produto) { novaQuantidade ->
                     produto.quantidade = novaQuantidade
-                    atualizarConteudoProduto(nomeProdutoPopup, categoriaProdutoPopup, precoProdutoPopup, quantidadeProdutoPopup, validadeProdutoPopup)
+                    atualizarConteudoProduto(nomeProdutoPopup, categoriaProdutoPopup,descricaoProduto, precoProdutoPopup, quantidadeProdutoPopup, validadeProdutoPopup)
                 }
                 is ProdutosActivity -> (requireActivity() as ProdutosActivity).showSaidaDialog(produto) { novaQuantidade ->
                     produto.quantidade = novaQuantidade
-                    atualizarConteudoProduto(nomeProdutoPopup, categoriaProdutoPopup, precoProdutoPopup, quantidadeProdutoPopup, validadeProdutoPopup)
+                    atualizarConteudoProduto(nomeProdutoPopup, categoriaProdutoPopup, precoProdutoPopup, quantidadeProdutoPopup, validadeProdutoPopup,descricaoProduto)
                 }
             }
         }
@@ -74,12 +73,16 @@ class ProdutoBottomSheetFragment(private val produto: Produto) : BottomSheetDial
         categoriaProduto: TextView,
         precoProduto: TextView,
         quantidadeProduto: TextView,
-        validadeProduto: TextView
+        validadeProduto: TextView,
+        descricaoProduto: TextView
+
     ) {
         nomeProduto.text = produto.nome
         categoriaProduto.text = "Categoria: ${produto.categoria}"
         precoProduto.text = "Preço: ${produto.preco}"
         quantidadeProduto.text = "Quantidade: ${produto.quantidade}"
         validadeProduto.text = "Validade: ${produto.validade ?: "Não Informada"}"
+        descricaoProduto.text = "Descrição: ${produto.descricao}"
+
     }
 }
